6047443faef314440109e4c6e1b8c283407abf4f
ShubhamSKadam <shubhamskadam99@outlook.com>
2023-04-17 11:04:01 +0530

    updated

--- index.js
+++ index.js
@@ -2,37 +2,64 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const USERS = [];
+
+const QUESTIONS = [
+  {
+    title: "Two states",
+    description: "Given an array, return the max of the array",
+    testCases: [
+      {
+        input: "[1,2,3,4,5]",
+        output: "5",
+      },
+    ],
+  },
+];
+
+const SUBMISSIONS = [];
+
 app.post("/signup", (req, res) => {
-  // Add logic to decode body
-  // body should have email and password
+  // check if user with email already exists
+  const existingUser = USERS.find((user) => user.email === email);
 
-  // store email and password ( as is for me ) is the USERS array above ( only if the user with the given email doesn't exist).
-
-  // return 200 status code to the client
-
-  res.send("Hello World!");
+  if (existingUser) {
+    res.status(400).send("User with email already exists"); // return 400 error if user already exists
+  } else {
+    USERS.push({ email, password }); // add new user to the users array
+    res.status(200).send("User created successfully"); // return 200 status code if user was created successfully
+  }
 });
 
 app.post("/login", (req, res) => {
-  // Add logic to decode body
-  // body should have email and password
+  const { email, password } = req.body; // decode body to get email and password fields
 
-  // Check if the user with the given email exists in the USERS array
-  // Also ensure the password is the same
+  // check if the user with email already exists in the USERS array
+  const user = USERS.find((user) => user.email === email);
 
-  // If the password is the same, retuurn back the 200 status to the client
-  // Also send back the token ( any random string will do for now)
-  // If the password is not the same, return back 401 status code to the client.
-
-  res.send("Hello World from route2!");
+  // If user does not exist or passowrd is incorrect
+  if (!user || user.password !== password) {
+    res.status(401).send("Invalid email or password");
+  } else {
+    // If password is correct, create a token and send it to client along with a 200 status code
+    const token = "randomString123"; // generate random token
+    res.status(200).json({ token }); // send token as JSON response;
+  }
 });
 
 app.get("/questions", (req, res) => {
-  res.send("Hello World from route2!");
+  const Questions = QUESTIONS;
+  res.json(userQuestions);
 });
 
 app.get("/submissions", (req, res) => {
+  // return the users submission for this problem
   res.send("Hello World from route2!");
+});
+
+app.post("/submissions", function (req, res) {
+  // let the user submit a problem , randomly accept or reject the the solution
+  // store the submission in the submission array above.
 });
 
 app.listen(port, () => {
